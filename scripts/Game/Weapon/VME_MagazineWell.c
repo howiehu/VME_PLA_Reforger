a7668bdb945f6b6412bb7d8ec1210aa4d99a6ef7
Alex.XP <huhao@outlook.com>
2022-05-29 22:13:14 +0800

    [tweak] change QSZ-54 magazine well

--- scripts/Game/Weapon/VME_MagazineWell.c
+++ scripts/Game/Weapon/VME_MagazineWell.c
@@ -4,3 +4,10 @@ VME_MagazineWellQBZ81 VME_MagazineWellQBZ81Source;
 class VME_MagazineWellQBZ81 : BaseMagazineWell
 {
 };
+
+class VME_MagazineWellTokarevTT33Class {}
+VME_MagazineWellTokarevTT33 VME_MagazineWellTokarevTT33Source;
+
+class VME_MagazineWellTokarevTT33 : BaseMagazineWell
+{
+};
